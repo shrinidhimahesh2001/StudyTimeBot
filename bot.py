05dc3cb08e19fd01c34705e05126ed194f9b522c
shrinidhimahesh2001 <66459886+shrinidhimahesh2001@users.noreply.github.com>
2021-02-21 21:13:30 +0530

    Added Bot and Env files

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+import os
+import datetime
+import requests
+from discord.ext import commands
+
+client= commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+    print('Hello!')
+
+@client.command()
+async def hi(ctx):
+    await ctx.channel.send(':nerd: Hello there!') 
+
+@client.command()
+async def timenow(ctx):
+    timestamp = datetime.datetime.now()
+    await ctx.send(f'{ctx.author.name},the time now is {timestamp.strftime(r"%I:%M %p")}')
+#@client.command()
+#async def stay(ctx):
+    #timestamp=datetime.datetime.now().time()
+    #if(timestamp>9):
+       # await ctx.send(f'{ctx.author.name},you should leave now. Go get some sleep')
+   # else:
+       # await ctx.send(f'{ctx.author.name},you hang around and study a bit more')
+
+#client.add_command(hi)
+client.run(os.getenv('TOKEN'))
